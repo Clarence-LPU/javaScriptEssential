c371dacdc25f7aa7435d114bb11618ff12a4141b
Clarence-LPU <csarmiento@lpubatangas.edu.ph>
2025-03-11 01:15:05 -0400

    Updated followers_count.html and followers_count.js, added reset button and functionality

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -4,16 +4,24 @@ function increaseCount() {
     count++; // Increment the count by 1
     displayCount(); // Display the count
     checkCountValue(); // Check count value and display messages
-  }
+};
+
+function resetCount() {
+    count = 0; // Reset count to 0
+    displayCount();
+    checkCountValue();
+}
 
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
-}
+};
 
 function checkCountValue() {
-    if (count === 10) {
+    if (count === 0) {
+        alert("Followers count has been reset.");
+    } else if (count === 10) {
       alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
-  }
+};
