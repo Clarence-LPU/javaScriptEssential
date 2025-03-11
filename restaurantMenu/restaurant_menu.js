0dcaa92c13078d56c8fa7df433afe8eeca9c9c6e
Clarence-LPU <csarmiento@lpubatangas.edu.ph>
2025-03-11 03:09:50 -0400

    Updated restaurant_menu.html and restaurant_menu.js, added prices in the menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $75.50', 'Pasta- $45.95', 'Burger- $40', 'Salmon- $99.99'];
+const dessertMenu = ['Cake- $120', 'Ice Cream- $50', 'Pudding- $25', 'Fruit Salad- $40'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
